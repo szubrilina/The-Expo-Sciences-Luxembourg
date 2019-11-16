289d5fe86041ceaab6649f47bf1c6e45c6b883b3
Zubrilina Sofia <30294376+szubrilina@users.noreply.github.com>
2019-11-16 09:56:37 +0300

    Add files via upload

---
+++ project/Find_Clique/MainPage.xaml.cpp
@@ -0,0 +1,27 @@
+﻿//
+// MainPage.xaml.cpp
+// Реализация класса MainPage.
+//
+
+#include "pch.h"
+#include "MainPage.xaml.h"
+
+using namespace Find_Clique;
+
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+
+// Шаблон элемента пустой страницы задокументирован по адресу http://go.microsoft.com/fwlink/?LinkId=234238
+
+MainPage::MainPage()
+{
+	InitializeComponent();
+}
